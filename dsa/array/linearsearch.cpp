d0ad20d3bd260cc4c6ab548c57a5cdb3154d9980
Abhishek kumar <flashtoak@gmail.com>
2021-09-24 20:35:17 +0530

    linearsearch with input key in c and c++

--- dsa/array/linearsearch.cpp
+++ dsa/array/linearsearch.cpp
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
+using namespace std;
 int linearsearch(int arr[],int size,int key){
 	for(int i=0;i<size;i++){
 		//check and return index of key element if it is present
@@ -10,19 +10,19 @@ int linearsearch(int arr[],int size,int key){
 	return -1;
 
 }
-void main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
 	int arr[] = {2,5,6,3,6,9,5,3,4,8};
 
 	int size = sizeof(arr)/sizeof(int);
-	printf("Enter key to Search: ");
+	cout<<"Enter key to Search: ";
 	int key; //= (int)((argv[1][0]) - 48);  //using command line arguments accesing first character in the char array 
-	scanf("%d",&key);
+	cin>>key;
 	int x = linearsearch(arr,size,key);
 	if(x!=-1){
-		printf("\nIndex of key is : %d ",x);
+		cout<<"index of key is : "<<x<<endl;
 	}
 	else{
-		printf("\nkey not found!");
+		cout<<"key not found!"<<endl;
 	}
 
 
