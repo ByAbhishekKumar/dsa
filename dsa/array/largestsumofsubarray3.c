5b1646ba7e8eec3f7d4537e8dee257806670c552
Abhishek kumar <flashtoak@gmail.com>
2021-09-25 21:26:31 +0530

    largestsumofarray with time O(n), kandane's algorithm

---
+++ dsa/array/largestsumofsubarray3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+int largestSumofSubarray(int arr[], int n)
+{
+    int curr=0;int max=0;
+    for (int i = 0; i < n; i++)
+    {
+        curr = curr + arr[i];
+        if(curr<0){
+            curr = 0;
+        }
+        if(max < curr){
+            max = curr;
+        }
+    }
+    
+    return max;
+}
+
+int main()
+{
+    int arr[100] = {0};
+    int size, large;
+    printf("enter size: ");
+    scanf("%d", &size);
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    large = largestSumofSubarray(arr, size);
+    printf("largest sum of subarray is: %d", large);
+    return 0;
+}
