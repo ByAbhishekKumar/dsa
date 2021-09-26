ae0a9b44df0c794bb69a2f1b134354a6cca608fd
Abhishek kumar <flashtoak@gmail.com>
2021-09-26 15:32:33 +0530

    bubblesort optimised with flag,  time remain same O(N)

--- dsa/sort/bubblesort.c
+++ dsa/sort/bubblesort.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 int bubbleSort(int arr[], int n)
 {
-
+    int flag = 0;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-
+                flag = 1;
                 int temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
             }
         }
+        if (flag = 0)
+            return 0;
     }
 }
 int main()
 {
-    int arr[6] = {20, -2, 6, -10, 8, 5};
-    int x = 6;
+    int arr[10] = {20, -2, 6, -10, -3, 5, -55, 15, 12, 7};
+    int x = 10;
     bubbleSort(arr, x);
     printf("%d", x);
     printf("\n");
 
     for (int i = 0; i < x; i++)
-    {
+
         printf(" %d ", arr[i]);
-    }
+
     printf("\n");
 }
